dbb4ffb73ce7e0127c85591f3f5cfe8f533c3aa8
Chema Fuentes <chema.fuentes@gmail.com>
2020-12-18 10:06:09 +0100

    Python - ejercicio 1

---
+++ python_challenge.py
@@ -0,0 +1,20 @@
+class Persona:
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+
+    def presentation(self):
+        print(f"Hola! Soy {self.nombre} y tengo {self.edad} años")
+
+
+class Trabajador(Persona):
+    def __init__(self, nombre, edad, departamento, puesto):
+        Persona.__init__(self, nombre, edad)
+
+        self.departamento = departamento
+        self.puesto = puesto
+
+
+nombre = 'Alberto'
+persona_1 = Persona(nombre, 20)
+persona_1.presentation()
