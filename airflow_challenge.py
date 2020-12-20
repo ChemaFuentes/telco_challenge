62ef6c91e508cf8ced8a4cc201106d2fdeb4e2b3
Chema Fuentes <chema.fuentes@gmail.com>
2020-12-20 11:56:40 +0100

    Airflow - ejercicio 3

--- airflow_challenge.py
+++ airflow_challenge.py
@@ -24,3 +24,22 @@ start = DummyOperator(task_id='start', dag=dag)
 end = DummyOperator(task_id='end', dag=dag)
 
 start >> end
+
+N = 10
+
+dag_2 = DAG(
+    'ejercicios-airflow-3',
+    default_args=default_args,
+    description='DAG de ejercicios de airflow: pares e impares',
+    schedule_interval=_SCHEDULE_INTERVAL,
+)
+
+tasks = [DummyOperator(task_id='task_' + str(i+1), dag=dag_2) for i in list(range(0, N))]
+
+tasks_pares = tasks[1::2]
+
+tasks_impares = tasks[0::2]
+
+for task in tasks_pares:
+    tasks_impares >> task
+
